de0efa7997052c733fd13f5cf3d96cad476c44ac
rapinchuk <rapinchuk1998@gmail.com>
2022-11-13 00:24:21 +0200

    add fetch data from API

--- app/js/common.js
+++ app/js/common.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 	const input = document.querySelector("#input")
 	const searchBtn = document.querySelector(".search_btn")
-	
+	const list = document.querySelector(".result_list")
 	searchBtn.addEventListener("click", getData)
 
 	function getData(e){
@@ -11,7 +11,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		fetch(url)
 			.then((resp) => resp.json())
 			.then(function (data) {
-				console.log(data)
+				console.log(data);
+				data.forEach(item => {
+					const country = item.country
+					const name = item.name
+					const link = item.web_pages[0]
+					const div = createNode(country,name,link)
+					append(div)
+				});
+			
 			})
 			.catch(function (error) {
 				console.log(error);
@@ -19,6 +27,34 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 	}
 
+
+	// function render(data){
+	// 	data.forEach(element => {
+	// 		element.map(function(item) {
+	// 			const country = item.country
+	// 			const name = item.name
+	// 			const link = item.link
+	// 			createNode()
+				
+	// 		});
+	// 	});
+	// }
+	function append(el) {
+
+		return list.appendChild(el)
+	}
+	function createNode(country,name,link) {
+		const div = document.createElement("div");
+		div.classList.add('result_item')
+		div.classList.add('item')
+		div.innerHTML = `<span class="item_country">${country}</span>
+		<span class="item_name">${name}</span>
+		<a target="_blank" href=${link} class="item_link">Go to Site..</a>
+		<span class="item_save"><input type="checkbox" name="save" id="save-item">&nbsp;save</span>`
+		// console.log(div)
+		return div
+		
+	}
 	// fetch("http://universities.hipolabs.com/search?country=Ukraine")
 	// 	.then((resp) => resp.json())
 	// 	.then(function (data) {

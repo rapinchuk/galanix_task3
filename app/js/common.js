755265895341bf5bf53833f6040fe8821eb9fbd1
rapinchuk <rapinchuk1998@gmail.com>
2022-11-13 04:28:11 +0200

    add favorite function

--- app/js/common.js
+++ app/js/common.js
@@ -4,36 +4,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	const list = document.querySelector(".result_list")
 	const reset = document.querySelector(".search_reset")
 	const description = document.querySelector(".result_description")
+	const counterNode = document.querySelector(".favorite_counter")
+	const favorite = document.querySelector(".favorite")
+	const storage = window.localStorage
+	const favoriteTitle = document.querySelector(".favorite-title")
+	let counter= storage.length;
+	let checkbox;
+	
 
-	function getData(e){
-		e.preventDefault()
-		
-		const key = input.value
-		const url = `http://universities.hipolabs.com/search?country=${key}`
-		fetch(url)
-			.then((resp) => resp.json())
-			.then(function (data) {
-				// console.log(data);
-				clear()
-				data.forEach(item => {
-					const country = item.country
-					const name = item.name
-					const link = item.web_pages[0]
-					const div = createNode(country,name,link)
-					append(div)
-				});
-				reset.style.cssText = "display: block;"
-				reset.addEventListener("click",resetHendler)
-				// const checkbox = document.
-				
-			
-			})
-			.catch(function (error) {
-				console.log(error);
-			});
 
-	}
-
+	counterNode.innerHTML = counter
 
 	
 	function append(el) {
@@ -64,11 +44,105 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	}
 	function resetHendler(e) {
 		e.preventDefault()
+		favoriteTitle.style.cssText = "display:none;"
 		reset.style.cssText = "display: none;"
 		clear()
 		description.style.cssText = "display: inline-block;"
 		
 	}
+
+	function getData(e){
+		e.preventDefault()
+		favoriteTitle.style.cssText = "display: none;"
+		if(input.value){
+		const key = input.value
+		const url = `http://universities.hipolabs.com/search?country=${key}`
+		fetch(url)
+			.then((resp) => resp.json())
+			.then(function (data) {
+				// console.log(data);
+				clear()
+				data.forEach(item => {
+					const country = item.country
+					const name = item.name
+					const link = item.web_pages[0]
+					const div = createNode(country,name,link)
+					append(div)
+				});
+				reset.style.cssText = "display: block;"
+				
+				checkbox = document.querySelectorAll("#save-item")
+				checkbox.forEach(el => {
+					el.addEventListener("change",checkboxHendler)
+				});
+				
+			
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}
+
+	}
+
+
+
+	function checkboxHendler(e) {
+		
+		if (this.checked) {
+			
+			
+			let value = e.target.closest(".result_item ").innerHTML
+			let key = e.target.closest(".result_item ").querySelector(".item_link").getAttribute('href')
+			storage.setItem(key,value)
+			counter = storage.length
+			counterNode.innerHTML = counter
+		  	console.log(e.target);
+	
+		} else {
+			const key = e.target.closest(".result_item ").querySelector(".item_link").getAttribute('href')
+			
+			counterNode.innerHTML = counter
+			storage.removeItem(key)
+			counter = storage.length
+			counterNode.innerHTML = counter
+			console.log(storage);
+		  	console.log("Checkbox is not checked..");
+		}
+		
+	  }
+
+	function favoriteHendler() {
+		reset.style.cssText = "display: block;"
+		favoriteTitle.style.cssText = "display: inline-block;"
+		renderFavorite()
+		checkbox = document.querySelectorAll("#save-item")
+		checkbox.forEach(el => {
+			el.checked = true;
+			el.addEventListener("change",checkboxHendler)
+		});
+		
+	}
+	
+	function renderFavorite() {
+		clear()
+		for (let i = 0; i < storage.length; i++) {
+			const div = document.createElement("div");
+			div.classList.add('result_item')
+			div.classList.add('item')
+			div.innerHTML = storage.getItem(storage.key(i))
+			console.log(div)
+			append(div)
+		}
+		
+	}
+
+
+
+	reset.addEventListener("click",resetHendler)
+	favorite.addEventListener("click",favoriteHendler)
 	searchBtn.addEventListener("click", getData)
 	
+	// storage.clear()
+	// 
   });
